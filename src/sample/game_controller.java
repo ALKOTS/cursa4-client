6b03fc67f1c2cc56f693f9eff18df1c8ece3afc6
ALKOTS <71733907+ALKOTS@users.noreply.github.com>
2021-04-05 09:51:14 +0300

    Added async timer

--- src/sample/game_controller.java
+++ src/sample/game_controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 
 public class game_controller {
@@ -15,8 +16,9 @@ public class game_controller {
     @FXML
     public TextField ansTxt;
 
+
     @FXML
-    public Label timeLbl;
+    public ProgressBar ansTimer;
 
 
     public String right_answer="ss";
@@ -24,6 +26,24 @@ public class game_controller {
 
     public void initialize(){
         qsLbl.setText("What is ss?");
+        ansTimer.setProgress(0);
+
+        new Thread(new Runnable() {
+            long startTime=System.currentTimeMillis();
+            long endTime=startTime+5000;
+            @Override
+            public void run() {
+                while (startTime<endTime){
+                    ansTimer.setProgress((5000-endTime+startTime)/10000);
+                    System.out.println(5000-endTime+startTime);
+                    startTime=System.currentTimeMillis();
+
+                }
+            }
+        }).start();
+
+
+
     }
 
     public void btnPress(javafx.event.ActionEvent actionEvent) {
@@ -36,6 +56,8 @@ public class game_controller {
         }
 
     }
+
+
 
 
 
