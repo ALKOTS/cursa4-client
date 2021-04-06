ea8b7cf487667cf8ae17ed924f8c2c691a62208a
ALKOTS <71733907+ALKOTS@users.noreply.github.com>
2021-04-06 13:52:12 +0300

    Adding question list support

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main extends Application {
+
+    public static ArrayList<ArrayList<String>> questions=new ArrayList<ArrayList<String>>();
+
 
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        ArrayList<String> question=new ArrayList<String>();
+        question.add("What is ss?");
+        question.add("ss");
+        questions.add(question);
         primaryStage.setTitle("Что? Где? Когда?");
         primaryStage.setScene(SceneChanger.changeScene("main_menu"));
         primaryStage.show();
--- src/game_controller.java
+++ src/game_controller.java
@@ -6,6 +6,8 @@ import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.concurrent.Task;
 
+import java.util.ArrayList;
+
 public class game_controller {
     @FXML
     public Label qsLbl, timeLbl;
@@ -19,37 +21,35 @@ public class game_controller {
     @FXML
     public ProgressBar timeBar;
 
-
+    ArrayList<ArrayList<String>> questions=Main.questions;
 
     public String right_answer="ss";
 
+    public my_timer mt;
 
     public void initialize()  {
 
         qsLbl.setText("What is ss?");
         timeBar.setProgress(0);
 
-
-        my_timer mt=new my_timer(){
-            int count=0;
+        mt=new my_timer(){
+            int count=5;
             @Override
             void run() {
-                  timeLbl.setText(String.valueOf(count++));
+                if(count>=0) {
+                    timeLbl.setText(String.valueOf(count--));
+                }else{
+                    outOfTime();
+                }
             }
-
         };
         mt.start();
-
-
-
-
-
-
 
     }
 
     public void btnPress(javafx.event.ActionEvent actionEvent) {
         System.out.println(ansTxt.getText());
+        mt.stop();
         if(ansTxt.getText().equals(right_answer)){
             qsLbl.setText("Mocha");
         }
@@ -57,6 +57,11 @@ public class game_controller {
             qsLbl.setText("Govno");
         }
 
+    }
+
+    public void outOfTime(){
+        System.out.println("ss");
+        mt.stop();
     }
 
 
