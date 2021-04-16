720a2a85921ba67d21852c9280faee90d79719be
ALKOTS <71733907+ALKOTS@users.noreply.github.com>
2021-04-16 15:45:32 +0300

    Ну все, создал папочку с сервером
    99% работы

---
+++ Server/src/main/java/server/cursa4_server/Cursa4ServerApplication.java
@@ -0,0 +1,13 @@
+package server.cursa4_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursa4ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursa4ServerApplication.class, args);
+    }
+
+}
---
+++ Server/src/test/java/server/cursa4_server/Cursa4ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package server.cursa4_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursa4ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
